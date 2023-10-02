e3784562edfd55a88da7a3ab712aa4182030a004
Elyse Lewis <22716917+elyseoc@users.noreply.github.com>
2023-10-02 10:30:15 -0700

    cleaned up and EHD index calcs added

--- EHD-ETC-indexbuilds.R
+++ EHD-ETC-indexbuilds.R
@@ -4,6 +4,9 @@
 # Purpose: POST EHD-ETC-indexbuilds - explore and analyze sensitivity re: urban/rural correlations
 # PRE-code: "/PhD Work/WSDOT Equity/Analysis/transpoequity_indices/EHD-ETC-indexbuilds.R"
 # source("/PhD Work/WSDOT Equity/Analysis/transpoequity_indices/EHD-ETC-indextests.R", echo=T)
+#
+# NOTES:
+# partially derivative of code developed by j.f. found at https://github.com/jfrostad/ehd_mapsense/tree/main/code
 #***********************************************************************************************************************
 
 ##----CONFIG------------------------------------------------------------------------------------------------------------
@@ -28,6 +31,8 @@ library(pacman)
 pacman::p_load(readxl, here, data.table, dplyr, tidyr,
                sf, tidycensus)
 
+#***********************************************************************************************************************
+
 ##----FUNCTIONS------------------------------------------------------------------------------------------------------------
 decile_scale <- function(x) {
   x_jittered <- x + runif(length(x), -1e-10, 1e-10)  # Add small jitter
@@ -40,20 +45,18 @@ min_max_scale <- function(x) {
   (x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
 }
 
+#***********************************************************************************************************************
 
 ##----ETC CALCS------------------------------------------------------------------------------------------------------------
 etc <- read_sf(here(file.path(
   data.in, 'USDOT_ETC/DOT_Index_State_5_3'),'DOT_Index_State_5_3.shp')) %>%
-  subset(stbbr=='WA')
-
-#rename tracts FIPS code column name to GEOID to match other dfs
-colnames(etc)[colnames(etc) == "trctfp"] <- "GEOID"
-#remove extra first 9 characters
-etc <- etc %>% mutate('GEOID'=substring(GEOID, 10))
+  subset(stbbr=='WA') %>% 
+  #rename tracts FIPS code column name to GEOID to match other dfs
+  dplyr::rename(GEOID = trctfp)
 
 
-# Select the variables for which you want to calculate z-scores
-etc_calcs_base <- etc[, c(
+# Select the base variables needed for calcs + retain the GEOID
+etc_all_vars <- c(
   "pctnvh", "avgcmm", "trnfrq", "jb45dr", "drvpoi",
   "wlkpoi", "avghht", "ftltsp", "ozn", "pm25",
   "dslpm", "cncrtx", "hzrdst", "txcrls", "trtdsp",
@@ -63,16 +66,16 @@ etc_calcs_base <- etc[, c(
   "pndplm", "pnmply", "phstnr", "phbrd7", "pnnsrd",
   "pnntrn", "endnql", "p65ldr", "p17yng", "pdsb",
   "plmng", "pmblhm", "annlls", "extht", "extrmp",
-  "drghtd", "pctnnd", "mnmp"
-)]
-etc_calcs_base <- st_drop_geometry(etc_calcs_base) #drop geometry to allow for numerical calcs
+  "drghtd", "pctnnd", "mnmp")
+
+etc <- st_drop_geometry(etc) #drop geometry to allow for numerical calcs
 #reverse the distributions for "Frequency of Transit Services per Sq Mi" and "Jobs within a 45-min Drive" so they add appropriately to the disadvantage estimation
 etc$trnfrq <- max(etc$trnfrq) - etc$trnfrq + min(etc$trnfrq)
 etc$jb45dr <- max(etc$jb45dr) - etc$jb45dr + min(etc$jb45dr)
 
 
 # Calculate z-scores for the selected variables
-z_scores <- scale(etc_calcs_base)
+z_scores <- scale(etc[, etc_all_vars])
 
 
 # Shift all z-scores to be positive
@@ -81,29 +84,29 @@ min_z_score <- min(z_scores)
 shifted_z_scores <- z_scores - min_z_score
 
 # Add the shifted z-scored variables to the 'etc' data frame
-colnames(shifted_z_scores) <- paste0(colnames(etc_calcs_base), "_z")
-etc_calcs <- cbind(etc$GEOID, etc_calcs_base, shifted_z_scores)
+colnames(shifted_z_scores) <- paste0(colnames(z_scores), "_z")
+etc <- cbind(etc[c(1)], etc[, etc_all_vars], shifted_z_scores)
 
 
 # generate min-max scaled variables
-min_max_scaled_vars <- as.data.frame(apply(etc_calcs_base, 2, min_max_scale))
+min_max_scaled_vars <- as.data.frame(apply(etc[, etc_all_vars], 2, min_max_scale))
 min_max_scaled_vars[is.na(min_max_scaled_vars)] <- 0 # again, no lead mines, need 0 coerced
 
 # Add the min-max scaled variables to the 'etc' data frame
-colnames(min_max_scaled_vars) <- paste0(colnames(etc_calcs_base), "_mm")
-etc_calcs <- cbind(etc_calcs, min_max_scaled_vars)
+colnames(min_max_scaled_vars) <- paste0(etc_all_vars, "_mm")
+etc <- cbind(etc, min_max_scaled_vars)
 
 
 # Perform decile scaling for the selected variables
-decile_scaled_vars <- as.data.frame(apply(etc_calcs_base, 2, decile_scale))
+decile_scaled_vars <- as.data.frame(apply(etc[, etc_all_vars], 2, decile_scale))
 
 # Add the decile scaled variables to the 'etc' data frame
-colnames(decile_scaled_vars) <- paste0(colnames(etc_calcs_base), "_d")
-etc_calcs <- cbind(etc_calcs, decile_scaled_vars)
+colnames(decile_scaled_vars) <- paste0(etc_all_vars, "_d")
+etc <- cbind(etc, decile_scaled_vars)
 
 
 #clean-up
-rm(decile_scaled_vars, etc_calcs_base, min_max_scaled_vars, shifted_z_scores, 
+rm(decile_scaled_vars, min_max_scaled_vars, shifted_z_scores, 
    z_scores, min_z_score)
 gc()
 
@@ -112,72 +115,71 @@ gc()
 
 # Create a new variable for the subindex of transportation access for each standardization type
 # then scale each accordingly
-etc_calcs$trnsac_z <- rowSums(etc_calcs[, c(
+etc$trnsac_z <- rowSums(etc[, c(
   "pctnvh_z", "avgcmm_z", "trnfrq_z", "jb45dr_z", "drvpoi_z", "wlkpoi_z"
 )])
-etc_calcs$trnsac_z <- scale(etc_calcs$trnsac_z, center = TRUE, scale = TRUE)
+etc$trnsac_z <- scale(etc$trnsac_z, center = TRUE, scale = TRUE)
 
-etc_calcs$trnsac_mm <- rowSums(etc_calcs[, c(
+etc$trnsac_mm <- rowSums(etc[, c(
   "pctnvh_mm", "avgcmm_mm", "trnfrq_mm", "jb45dr_mm", "drvpoi_mm", "wlkpoi_mm"
 )])
-etc_calcs$trnsac_mm <- min_max_scale(etc_calcs$trnsac_mm)
+etc$trnsac_mm <- min_max_scale(etc$trnsac_mm)
 
-etc_calcs$trnsac_d <- rowSums(etc_calcs[, c(
+etc$trnsac_d <- rowSums(etc[, c(
   "pctnvh_d", "avgcmm_d", "trnfrq_d", "jb45dr_d", "drvpoi_d", "wlkpoi_d"
 )])
-etc_calcs$trnsac_d <- decile_scale(etc_calcs$trnsac_d)
+etc$trnsac_d <- decile_scale(etc$trnsac_d)
 
 
 # do the same for the Future Climate Risk variable
-etc_calcs$ftclrk_z <- rowSums(etc_calcs[, c(
+etc$ftclrk_z <- rowSums(etc[, c(
   "extht_z", "extrmp_z", "drghtd_z", "pctnnd_z"
 )])
-etc_calcs$ftclrk_z <- scale(etc_calcs$ftclrk_z, center = TRUE, scale = TRUE)
+etc$ftclrk_z <- scale(etc$ftclrk_z, center = TRUE, scale = TRUE)
 
-etc_calcs$ftclrk_mm <- rowSums(etc_calcs[, c(
+etc$ftclrk_mm <- rowSums(etc[, c(
   "extht_mm", "extrmp_mm", "drghtd_mm", "pctnnd_mm"
 )])
-etc_calcs$ftclrk_mm <- min_max_scale(etc_calcs$ftclrk_mm)
+etc$ftclrk_mm <- min_max_scale(etc$ftclrk_mm)
 
-etc_calcs$ftclrk_d <- rowSums(etc_calcs[, c(
+etc$ftclrk_d <- rowSums(etc[, c(
   "extht_d", "extrmp_d", "drghtd_d", "pctnnd_d"
 )])
-etc_calcs$ftclrk_d <- decile_scale(etc_calcs$ftclrk_d)
+etc$ftclrk_d <- decile_scale(etc$ftclrk_d)
 
 
 #calculate each component index percentile rank score
 
 # transportation insecurity
-etc_calcs$trncmpr_z <- rowSums(etc_calcs[, c("avghht_z", "ftltsp_z", "trnsac_z")])
-etc_calcs$trncmpr_mm <- rowSums(etc_calcs[, c("avghht_mm", "ftltsp_mm", "trnsac_mm")])
-etc_calcs$trncmpr_d <- rowSums(etc_calcs[, c("avghht_d", "ftltsp_d", "trnsac_d")])
+etc$trncmpr_z <- rowSums(etc[, c("avghht_z", "ftltsp_z", "trnsac_z")])
+etc$trncmpr_mm <- rowSums(etc[, c("avghht_mm", "ftltsp_mm", "trnsac_mm")])
+etc$trncmpr_d <- rowSums(etc[, c("avghht_d", "ftltsp_d", "trnsac_d")])
 
 # Compute the percentile rank of the sums for each re-scaling technique
-#DOUBLE THEM HERE for ease later in code
-etc_calcs <- etc_calcs %>%
+etc <- etc %>%
   mutate(
-    trncmpr_z = percent_rank(trncmpr_z)*2,
-    trncmpr_mm = percent_rank(trncmpr_mm)*2,
-    trncmpr_d = percent_rank(trncmpr_d)*2
+    trncmpr_z = percent_rank(trncmpr_z),
+    trncmpr_mm = percent_rank(trncmpr_mm),
+    trncmpr_d = percent_rank(trncmpr_d)
   )
 
 # environmental burden
-etc_calcs$evncmpr_z <- rowSums(etc_calcs[, c(
+etc$evncmpr_z <- rowSums(etc[, c(
   "ozn_z", "pm25_z", "dslpm_z", "cncrtx_z", "hzrdst_z",
   "txcrls_z", "trtdsp_z", "rskmns_z", "clmns_z", "ldmns_z",
   "ppr80h_z", "hghvlr_z", "rlwys_z", "arprts_z", "prts_z", "imprdw_z"
 )])
-etc_calcs$evncmpr_mm <- rowSums(etc_calcs[, c(
+etc$evncmpr_mm <- rowSums(etc[, c(
   "ozn_mm", "pm25_mm", "dslpm_mm", "cncrtx_mm", "hzrdst_mm",
   "txcrls_mm", "trtdsp_mm", "rskmns_mm", "clmns_mm", "ldmns_mm",
   "ppr80h_mm", "hghvlr_mm", "rlwys_mm", "arprts_mm", "prts_mm", "imprdw_mm"
 )])
-etc_calcs$evncmpr_d <- rowSums(etc_calcs[, c(
+etc$evncmpr_d <- rowSums(etc[, c(
   "ozn_d", "pm25_d", "dslpm_d", "cncrtx_d", "hzrdst_d",
   "txcrls_d", "trtdsp_d", "rskmns_d", "clmns_d", "ldmns_d",
   "ppr80h_d", "hghvlr_d", "rlwys_d", "arprts_d", "prts_d", "imprdw_d"
 )])
-etc_calcs <- etc_calcs %>%
+etc <- etc %>%
   mutate(
     evncmpr_z = percent_rank(evncmpr_z),
     evncmpr_mm = percent_rank(evncmpr_mm),
@@ -186,16 +188,16 @@ etc_calcs <- etc_calcs %>%
 
 
 # health vulnerability
-etc_calcs$hltcmpr_z <- rowSums(etc_calcs[, c(
+etc$hltcmpr_z <- rowSums(etc[, c(
   "asthm_z", "cncr_z", "bldprs_z", "dbts_z", "mntlhl_z"
 )])
-etc_calcs$hltcmpr_mm <- rowSums(etc_calcs[, c(
+etc$hltcmpr_mm <- rowSums(etc[, c(
   "asthm_mm", "cncr_mm", "bldprs_mm", "dbts_mm", "mntlhl_mm"
 )])
-etc_calcs$hltcmpr_d <- rowSums(etc_calcs[, c(
+etc$hltcmpr_d <- rowSums(etc[, c(
   "asthm_d", "cncr_d", "bldprs_d", "dbts_d", "mntlhl_d"
 )])
-etc_calcs <- etc_calcs %>%
+etc <- etc %>%
   mutate(
     hltcmpr_z = percent_rank(hltcmpr_z),
     hltcmpr_mm = percent_rank(hltcmpr_mm),
@@ -204,22 +206,22 @@ etc_calcs <- etc_calcs %>%
 
 
 # social vulnerability
-etc_calcs$sclcmpr_z <- rowSums(etc_calcs[, c(
+etc$sclcmpr_z <- rowSums(etc[, c(
   "ppvrty_z", "pndplm_z", "pnmply_z", "phstnr_z", "phbrd7_z",
   "pnnsrd_z", "pnntrn_z", "endnql_z", "p65ldr_z", "p17yng_z",
   "pdsb_z", "plmng_z", "pmblhm_z"
 )])
-etc_calcs$sclcmpr_mm <- rowSums(etc_calcs[, c(
+etc$sclcmpr_mm <- rowSums(etc[, c(
   "ppvrty_mm", "pndplm_mm", "pnmply_mm", "phstnr_mm", "phbrd7_mm",
   "pnnsrd_mm", "pnntrn_mm", "endnql_mm", "p65ldr_mm", "p17yng_mm",
   "pdsb_mm", "plmng_mm", "pmblhm_mm"
 )])
-etc_calcs$sclcmpr_d <- rowSums(etc_calcs[, c(
+etc$sclcmpr_d <- rowSums(etc[, c(
   "ppvrty_d", "pndplm_d", "pnmply_d", "phstnr_d", "phbrd7_d",
   "pnnsrd_d", "pnntrn_d", "endnql_d", "p65ldr_d", "p17yng_d",
   "pdsb_d", "plmng_d", "pmblhm_d"
 )])
-etc_calcs <- etc_calcs %>%
+etc <- etc %>%
   mutate(
     sclcmpr_z = percent_rank(sclcmpr_z),
     sclcmpr_mm = percent_rank(sclcmpr_mm),
@@ -228,10 +230,10 @@ etc_calcs <- etc_calcs %>%
 
 
 #finally, climate & disaster risk burden
-etc_calcs$clmcmpr_z <- rowSums(etc_calcs[, c("annlls_z", "mnmp_z", "ftclrk_z")])
-etc_calcs$clmcmpr_mm <- rowSums(etc_calcs[, c("annlls_mm", "mnmp_mm", "ftclrk_mm")])
-etc_calcs$clmcmpr_d <- rowSums(etc_calcs[, c("annlls_d", "mnmp_d", "ftclrk_d")])
-etc_calcs <- etc_calcs %>%
+etc$clmcmpr_z <- rowSums(etc[, c("annlls_z", "mnmp_z", "ftclrk_z")])
+etc$clmcmpr_mm <- rowSums(etc[, c("annlls_mm", "mnmp_mm", "ftclrk_mm")])
+etc$clmcmpr_d <- rowSums(etc[, c("annlls_d", "mnmp_d", "ftclrk_d")])
+etc <- etc %>%
   mutate(
     clmcmpr_z = percent_rank(clmcmpr_z),
     clmcmpr_mm = percent_rank(clmcmpr_mm),
@@ -241,17 +243,10 @@ etc_calcs <- etc_calcs %>%
 
 
 # NOW FINAL index scores - HEIRARCHICAL
-etc_calcs$fnlrnk_z_h <- rowSums(etc_calcs[, c("trncmpr_z", "hltcmpr_z", "evncmpr_z", "sclcmpr_z", "clmcmpr_z")])
-etc_calcs$fnlrnk_mm_h <- rowSums(etc_calcs[, c("trncmpr_mm", "hltcmpr_mm", "evncmpr_mm", "sclcmpr_mm", "clmcmpr_mm")])
-etc_calcs$fnlrnk_d_h <- rowSums(etc_calcs[, c("trncmpr_d", "hltcmpr_d", "evncmpr_d", "sclcmpr_d", "clmcmpr_d")])
+etc$fr_z_h <- rowSums(etc[, c("trncmpr_z", "hltcmpr_z", "evncmpr_z", "sclcmpr_z", "clmcmpr_z")])
+etc$fr_mm_h <- rowSums(etc[, c("trncmpr_mm", "hltcmpr_mm", "evncmpr_mm", "sclcmpr_mm", "clmcmpr_mm")])
+etc$fr_d_h <- rowSums(etc[, c("trncmpr_d", "hltcmpr_d", "evncmpr_d", "sclcmpr_d", "clmcmpr_d")])
 
-# Compute the percentile rank of the mean percentile ranks for each grouping
-etc_calcs <- etc_calcs %>%
-  mutate(
-    fnlrnk_z_h = percent_rank(fnlrnk_z_h),
-    fnlrnk_mm_h = percent_rank(fnlrnk_mm_h),
-    fnlrnk_d_h = percent_rank(fnlrnk_d_h)
-  )
 
 
 # ALSO calcuate final scores given a non-hierarchical calc
@@ -273,24 +268,15 @@ all_other_vars <- c(
 
 
 # DOUBLE the transpo insecurity variable score, add all other values normally
-etc_calcs$fnlrnk_z_nh <- rowSums(etc_calcs[, c(paste0(trans_insec_vars, "_z"))]) * 2 +
-  rowSums(etc_calcs[, c(paste0(all_other_vars, "_z"))])
+etc$fr_z_nh <- rowSums(etc[, c(paste0(trans_insec_vars, "_z"))]) * 2 +
+  rowSums(etc[, c(paste0(all_other_vars, "_z"))])
 
-etc_calcs$fnlrnk_mm_nh <- rowSums(etc_calcs[, c(paste0(trans_insec_vars, "_mm"))]) * 2 +
-  rowSums(etc_calcs[, c(paste0(all_other_vars, "_mm"))])
+etc$fr_mm_nh <- rowSums(etc[, c(paste0(trans_insec_vars, "_mm"))]) * 2 +
+  rowSums(etc[, c(paste0(all_other_vars, "_mm"))])
 
-etc_calcs$fnlrnk_d_nh <- rowSums(etc_calcs[, c(paste0(trans_insec_vars, "_d"))]) * 2 +
-  rowSums(etc_calcs[, c(paste0(all_other_vars, "_d"))])
+etc$fr_d_nh <- rowSums(etc[, c(paste0(trans_insec_vars, "_d"))]) * 2 +
+  rowSums(etc[, c(paste0(all_other_vars, "_d"))])
 
-# calculate percentile rank
-etc_calcs <- etc_calcs %>%
-  mutate(
-    fnlrnk_z_nh = percent_rank(fnlrnk_z_nh),
-    fnlrnk_mm_nh = percent_rank(fnlrnk_mm_nh),
-    fnlrnk_d_nh = percent_rank(fnlrnk_d_nh)
-  )
-
-rm(trans_insec_vars, all_other_vars)
 
 
 # calculate binary disadvantaged/not variable
@@ -299,9 +285,23 @@ suffixes <- c("_z_h", "_mm_h", "_d_h", "_z_nh", "_mm_nh", "_d_nh")
 
 # Loop through the suffixes and create binary output variables
 for (suffix in suffixes) {
-  etc_calcs[[paste0("fnlrnki", suffix)]] <- ifelse(
-    etc_calcs[[paste0("fnlrnk", suffix)]] < 0.65, 0, 1)
+  etc[[paste0("frp", suffix)]] <- percent_rank(
+    etc[[paste0("fr", suffix)]])
+  etc[[paste0("fri", suffix)]] <- ifelse(
+    etc[[paste0("frp", suffix)]] < 0.65, 0, 1)
 }
+
+
+# subset to just final scores
+etc_out <- etc[, grep('fr', names(etc), value = T)]
+# add GEOID variable on
+etc_out <- cbind(etc[c(1)], etc_out)
+
+write.csv(etc_out, file.path(data.out, 'etc_scores'), row.names = FALSE)
+
+
+rm(etc_all_vars, trans_insec_vars, all_other_vars, suffix, suffixes, 
+   etc, etc_out)  #clean-up
 
 #***********************************************************************************************************************
 
@@ -386,18 +386,18 @@ ehd <- cbind(ehd, avg_calcs)
 # use EHD equation to calculate deciles of scores i.e. Hierarchical scores by scaling type
 for (version in scaling_types) {
   # Calculate the rank for the current version hierarchy
-  ehd[[paste0("ehd_fcs", version, "_h")]] <- decile_scale(
+  ehd[[paste0("fs", version, "_h")]] <-
     ((ehd[[paste0("avg_", 'envex', version)]] + 
         0.5*ehd[[paste0("avg_", 'enveff', version)]])/2) *
     ((ehd[[paste0("avg_", 'senspop', version)]] + 
-        ehd[[paste0("avg_", 'soecon', version)]])/2))
+        ehd[[paste0("avg_", 'soecon', version)]])/2)
   # now calculate Non-Hierarchical scores by scaling type, all decile ranked
     # NOTE: retain weighting scheme of 0.5 for enviro effects
-  ehd[[paste0("ehd_fcs", version, "_nh")]] <- decile_scale(
+  ehd[[paste0("fs", version, "_nh")]] <- 
     rowSums(ehd[, paste0(var_list$envex, version)], na.rm = T) +
       0.5 * rowSums(ehd[, paste0(var_list$enveff, version)], na.rm = T) +
       rowSums(ehd[, paste0(var_list$senspop, version)], na.rm = T) +
-      rowSums(ehd[, paste0(var_list$soecon, version)], na.rm = T))
+      rowSums(ehd[, paste0(var_list$soecon, version)], na.rm = T)
 }
 
 
@@ -410,11 +410,82 @@ suffixes <- c("_z_h", "_mm_h", "_d_h", "_z_nh", "_mm_nh", "_d_nh")
 
 # Loop through the suffixes and create binary output variables
 for (suffix in suffixes) {
-  ehd[[paste0("ehd_fcsi20", suffix)]] <- ifelse(
-    ehd[[paste0("ehd_fcs", suffix)]] < 9, 0, 1)
-  ehd[[paste0("ehd_fcsi40", suffix)]] <- ifelse(
-    ehd[[paste0("ehd_fcs", suffix)]] < 7, 0, 1)
+  ehd[[paste0("fsd", suffix)]]<- decile_scale(
+    ehd[[paste0("fs", suffix)]])
+  ehd[[paste0("fsi9", suffix)]] <- ifelse(
+    ehd[[paste0("fsd", suffix)]] < 9, 0, 1)
+  ehd[[paste0("fsi7", suffix)]] <- ifelse(
+    ehd[[paste0("fsd", suffix)]] < 7, 0, 1)
 }
 
 
-rm(avg_calcs, scaling_types, suffix, suffixes, var_group, version)  #clean-up
+# subset to just final scores
+ehd_out <- ehd[,grep('fcs', names(ehd), value = T)]
+# add GEOID variable on
+ehd_out <- cbind(ehd[c(1)], ehd_out)
+
+write.csv(ehd_out, file.path(data.out, 'ehd_scores'), row.names = FALSE)
+
+
+rm(avg_calcs, scaling_types, suffix, suffixes, var_group, version,
+   ehd, ehd_vars, ehd_out, var_list)  #clean-up
+
+#***********************************************************************************************************************
+
+
+##---SCRAP -------------------------------------------------------------------------------------------------------------
+# code originally from frey
+
+# clear memory
+rm(list=ls())
+
+#set opts
+set.seed(98118)
+options(scipen=999) #readability
+#use cairo to render instead of quartz (quartz causes big slowdowns with geom_sf)
+if(!identical(getOption("bitmapType"), "cairo") && isTRUE(capabilities()[["cairo"]])){
+  options(bitmapType = "cairo")
+}
+
+#set control flow params
+reload <- F #set true if you want to reprep all the data
+remap <- F #set true if you want to redraw all the data maps
+rerun_gsa <- F #set true if you are rerunning the global sensitivity analysis
+gsa_cores <- 1
+
+#version
+gsa_version <- 1 #first draft with params=
+gsa_version <- 2 #reduced noise
+gsa_version <- 3 #switch to MARC instead of just avg. rerunning with fixed PCA sign
+
+## Set core_repo location
+user            <- Sys.info()['user']
+main.dir         <- ifelse(Sys.info()["sysname"] == "Linux",
+                           file.path('/homes', user, '_code/ehd_mapsense/'),
+                           file.path('C:/Users', user, 'Documents/ehd_mapsense/'))
+my_repo <- file.path(main.dir, 'repo')
+setwd(my_repo)
+
+#load packages
+#TODO only relevant to running in linux on shared cluster
+package_lib    <- ifelse(.Platform$GUI=='RStudio',
+                         '/mnt/share/homes/jfrostad/_code/_lib/pkg_R_ehd',
+                         '/mnt/share/homes/jfrostad/_code/_lib/pkg_R_int')
+## Load libraries and  MBG project functions.
+.libPaths(package_lib)
+
+#TODO cleanup old packages
+pacman::p_load(readxl, snakecase, janitor, data.table, naniar, stringr, magrittr, scales, Hmisc,
+               ggtern, ggplot2, ggpubr, ggridges, ggrepel, ggdist, grid, gridExtra, RColorBrewer, #viz pkgs
+               sf, viridis, farver, reldist, ggnewscale, ggallin, biscale, cowplot,
+               tigris, tidycensus, ggcorrplot,
+               broom.mixed, ggstance, jtools, factoextra, scam,
+               COINr, randtoolbox, sensobol, #sens packages
+               stargazer,
+               parallel, pbmcapply,
+               cluster, ggdendro, #HCA packages
+               #caret, mlbench, randomForest, pls,
+               zoo)
+
+
+# #***********************************************************************************************************************
